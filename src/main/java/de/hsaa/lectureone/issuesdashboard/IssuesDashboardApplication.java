12336df070ea5e8bb81e026b96374c698f287e02
de313e <ufuk.altin@kiongroup.com>
2022-10-13 17:38:40 +0200

    initial Commit After Project Download

---
+++ src/main/java/de/hsaa/lectureone/issuesdashboard/IssuesDashboardApplication.java
@@ -0,0 +1,13 @@
+package de.hsaa.lectureone.issuesdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsaa/lectureone/issuesdashboard/IssuesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsaa.lectureone.issuesdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
